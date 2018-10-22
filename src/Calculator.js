e4443793af15afacece9e631f4bd784e1272e316
stewart86 <siwei.wong@gmail.com>
2018-10-22 13:45:40 +0800

    entering numbers with button

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,68 +7,85 @@ export default class Calculator extends Component {
     prop: PropTypes
   };
 
+  state = {
+    numbers: "0"
+  };
+
+  handleNumbers = newNumber => {
+    console.log("test", newNumber);
+    if (this.state.numbers === "0") {
+      console.log("if 0");
+      this.setState({
+        numbers: `${newNumber}`
+      });
+    } else {
+         this.setState({
+      numbers: `${this.state.numbers}${newNumber}`
+    });
+    }
+   
+  };
+
   render() {
     return (
       <Grid container>
         <Grid item sm />
         <Grid item sm>
-          <Paper 
-          elevation={10}
-            style={{backgroundColor:"darkgray"}}>
+          <Paper elevation={10} style={{ backgroundColor: "darkgray" }}>
             <TextField
-              defaultValue="This is the placeholder for the Calculator"
+              value={this.state.numbers}
               margin="dense"
               variant="outlined"
               fullWidth
             />
             <Grid container>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(7)}>7</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(8)}>8</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(9)}>9</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>/</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(4)}>4</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(5)}>5</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(6)}>6</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>X</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(1)}>1</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(2)}>2</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(3)}>3</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>-</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button onClick={() => this.handleNumbers(0)}>0</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>.</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>=</Button>
               </Grid>
               <Grid item sm={3}>
-                <Button>1</Button>
+                <Button>+</Button>
               </Grid>
             </Grid>
           </Paper>
